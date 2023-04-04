5baaf464cecd14ae142f15b889ec09c7e30cd08a
vmrozhkovsky <128725885+vmrozhkovsky@users.noreply.github.com>
2023-04-04 14:22:30 +0300

    Написан основной код

--- FirstApp.ConsoleApp/Program.cs
+++ FirstApp.ConsoleApp/Program.cs
@@ -1,10 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Как Вас зовут?");
-string name = Console.ReadLine();
-Console.WriteLine("Сколько Вам лет?");
-string age = Console.ReadLine();
-Console.WriteLine("Чем Вы занимаетесь на работе?");
-string work = Console.ReadLine();
-Console.WriteLine("Здравствуйте, {0}", name);
-Console.WriteLine("Вы мне сказали, что вас зовут {1}, Вам - {2} год и что на работе вы занимаетесь {0}", work, name, age);
-Console.WriteLine("Рад был познакомиться!");
+﻿
+using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string UserName;
+        byte UserAge;
+        string UserHaveSkillLine;
+        bool UserHaveSkill;
+        double UserSkillSize;
+
+        Console.WriteLine("Как Вас зовут?");
+        UserName = Console.ReadLine();
+        Console.WriteLine("Сколько Вам лет?");
+        UserAge = Convert.ToByte(Console.ReadLine());
+        Console.WriteLine("Имеете ли опыт программирования (Y/N)?");
+
+        UserHaveSkillLine = Console.ReadLine();
+        if (UserHaveSkillLine == "Y")
+        {
+            UserHaveSkill = true;
+            Console.WriteLine("Каков ваш уровень (1-10)?");
+            UserSkillSize = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Здравствуйте, {0}", UserName);
+            Console.WriteLine("Вы мне сказали, что вас зовут {1}, Вам - {2} год и у Вас есть опыт программирования, который Вы оценили на {0}", UserSkillSize, UserName, UserAge);
+            Console.WriteLine("Рад был познакомиться!");
+        }
+        else
+        {
+            UserHaveSkill = false;
+            Console.WriteLine("Здравствуйте, {0}", UserName);
+            Console.WriteLine("Вы мне сказали, что вас зовут {0}, Вам - {1} год и у Вас нет опыта программирования.", UserName, UserAge);
+            Console.WriteLine("Рад был познакомиться!");
+        }
+    }
+}
